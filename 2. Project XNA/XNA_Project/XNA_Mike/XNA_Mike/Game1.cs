4862dfd656fa0cece2a2af5d0fd276862abf0bad
Mike Kooistra <dragonsoft_nl@hotmail.com>
2014-11-18 15:15:07 +0100

    modify XNA_Mike Game1.cs 18-11-2014

--- 2. Project XNA/XNA_Project/XNA_Mike/XNA_Mike/Game1.cs
+++ 2. Project XNA/XNA_Project/XNA_Mike/XNA_Mike/Game1.cs
@@ -19,6 +19,7 @@ namespace XNA_Mike
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+//comment test
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
