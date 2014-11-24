381ba9d66546da724270006605bcf291313a20ad
Mathijs Arts <matonster@gmail.com>
2014-11-24 14:47:25 +0100

    Add TestProject code, Add Content 24-11-2014
    Taken testproject afgemaakt

--- 2. Project XNA/XNA_Project/XNA_Mathijs/XNA_Mathijs/Game1.cs
+++ 2. Project XNA/XNA_Project/XNA_Mathijs/XNA_Mathijs/Game1.cs
@@ -18,6 +18,9 @@ namespace XNA_Mathijs
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Vector2 mPosition = new Vector2(0, 0);
+        Texture2D mSpriteTexture;
+        SoundEffect mSoundEffect;
 
         public Game1()
         {
@@ -48,6 +51,8 @@ namespace XNA_Mathijs
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            mSpriteTexture = this.Content.Load<Texture2D>("Isaac_form1");
+            mSoundEffect = this.Content.Load<SoundEffect>("Isaac_sounds");
         }
 
         /// <summary>
@@ -71,6 +76,20 @@ namespace XNA_Mathijs
                 this.Exit();
 
             // TODO: Add your update logic here
+            UpdateMouse();
+
+            //checkMouseClick();
+
+            if (leftMouseClicked == true) // Don't bother collision checking unless mouse is clicked
+            {
+                OnMouseOver(); // Check if mouse is inside texture area
+                if (MouseOverSprite == true)
+                {
+                    PixelCheck(); // Check if individual pixel is non Alpha
+                    if (PixelDetected == true)
+                        mSoundEffect.Play();
+                }
+            } 
 
             base.Update(gameTime);
         }
@@ -84,8 +103,68 @@ namespace XNA_Mathijs
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(mSpriteTexture, mPosition, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+         // Mouse coordinates are initialized at 0, 0
+        Vector2 mousePosition = Vector2.Zero;
+        void UpdateMouse()
+        {
+            MouseState mouseState = Mouse.GetState();
+            IsMouseVisible = true; // Show the mouse cursor ( default is false )
+
+            // The mouse X and Y positions are returned relative to the top-left of the game window.
+            mousePosition.X = mouseState.X;
+            mousePosition.Y = mouseState.Y;
+
+        } // end UpdateMouse
+
+        bool MouseOverSprite = false;
+        void OnMouseOver()
+        {
+            if ((mousePosition.X >= mPosition.X) && mousePosition.X <= (mPosition.X + mSpriteTexture.Width) &&
+                    mousePosition.Y >= mPosition.Y && mousePosition.Y <= (mPosition.Y + mSpriteTexture.Height))
+                MouseOverSprite = true;
+            else MouseOverSprite = false;
+        } // end OnMouseOver
+
+
+        bool PixelDetected = false;
+        Vector2 pixelPosition = Vector2.Zero;
+        uint[] PixelData = new uint[1]; // Delare an Array of 1 just to store data for one pixel
+        void PixelCheck()
+        {
+            // Get Mouse position relative to top left of Texture
+            pixelPosition = mousePosition - mPosition;
+
+            // I know. I just checked this condition at OnMouseOver or we wouldn't be here
+            // but just to be sure the spot we're checking is within the bounds of the texture...
+            if (pixelPosition.X >= 0 && pixelPosition.X < mSpriteTexture.Width &&
+                pixelPosition.Y >= 0 && pixelPosition.Y < mSpriteTexture.Height)
+            {
+                // Get the Texture Data within the Rectangle coords, in this case a 1 X 1 rectangle
+                // Store the data in pixelData Array
+                mSpriteTexture.GetData<uint>(0, new Rectangle((int)pixelPosition.X, (int)pixelPosition.Y, (1), (1)), PixelData, 0, 1);
+
+                // Check if pixel in Array is non Alpha, give or take 20
+                if (((PixelData[0] & 0xFF000000) >> 24) > 20)
+                    PixelDetected = true;
+                else PixelDetected = false;
+            }
+        } // end PixelCheck
+
+        bool leftMouseClicked = false;
+        void checkMouseClick()
+        {
+            MouseState mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed) 
+                leftMouseClicked = true;
+            else leftMouseClicked = false;
+        } // end checkMouseClick
     }
+    
 }
